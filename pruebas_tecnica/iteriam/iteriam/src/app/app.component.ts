8e363a6a8957ca7673fa6165d04a63b97df983d7
viclomaro <viclomaro@gmail.com>
2020-07-25 13:01:31 +0200

    Busqueda por id ok

--- pruebas_tecnica/iteriam/iteriam/src/app/app.component.ts
+++ pruebas_tecnica/iteriam/iteriam/src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent implements OnInit {
 
   item = new Element;
   items = [];
+  idSearched: number;
+  textSearched: string;
 
   ngOnInit() {
     this.createElements();
@@ -20,9 +22,28 @@ export class AppComponent implements OnInit {
     for (let i = 0; i <= 4000; i++) {
       this.item.id = i;
       this.item.photo = `http://www.photo/${i}`;
-      this.item.text = 'lorem'
+      this.item.text = `random_lorem_ipsum_text${i}`
       this.items.push({ ...this.item });
     }
+  }
+
+  searchId() {
+    const itemSearched = this.items.find(item => item.id == this.idSearched);
+    this.items = [];
+    this.items.push(itemSearched);
+  }
+
+  deleteSearch() {
+    this.items = [];
+    this.createElements();
+    this.idSearched = null;
+    this.textSearched = null;
+  }
+
+  searchText() {
+    const itemSearched = this.items.find(item => item.text == this.textSearched);
+    this.items = [];
+    this.items.push(itemSearched);
   }
 
 }
