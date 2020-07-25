f327bcfce895e882181397a32486c3a6c94bc134
viclomaro <viclomaro@gmail.com>
2020-07-25 12:19:02 +0200

    Listado de elementos

--- pruebas_tecnica/iteriam/iteriam/src/app/app.component.ts
+++ pruebas_tecnica/iteriam/iteriam/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Element } from '../models/element';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iteriam';
+
+  item = new Element;
+  items = [];
+
+  ngOnInit() {
+    this.createElements();
+  }
+
+  createElements() {
+    for (let i = 0; i <= 4000; i++) {
+      this.item.id = i;
+      this.item.photo = `http://www.photo/${i}`;
+      this.item.text = 'lorem'
+      this.items.push({ ...this.item });
+    }
+  }
+
 }
+
+
+
---
+++ pruebas_tecnica/iteriam/iteriam/src/models/element.ts
@@ -0,0 +1,5 @@
+export class Element{
+    id: number;
+    photo: string;
+    text: string
+}
