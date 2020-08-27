752a397775feb8bfa16e92b129230103417bb85b
viclomaro <viclomaro@gmail.com>
2020-08-27 18:17:27 +0200

    NodeJs: instalación express y primeros pasos con carpeta public y midllewares

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,22 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.end('Estoy respondiendo a tu solicitud');
-});
+const express = require('express');
+const app = express();
 
 const port = 3000;
+app.use(express.static(__dirname + '/public'));
 
-server.listen(port, () => {
-    console.log('Escuchando solicitudes');
+
+app.get('/', (req, res) => {
+    res.send('Mi respuesta desde express');
+});
+
+app.get('/servicios', (req, res) => {
+    res.send('Estás en la página de servicios');
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () => {
+    console.log('Escuchando peticiones del servidor en el puerto', port);
 });
--- nodejs/frutas.js
+++
@@ -1,7 +0,0 @@
-const frutas = ['platano', 'manzana', 'platano', 'pera'];
-const dinero = 1000;
-
-module.exports = {
-    frutas: frutas,
-    dinero
-}
