67c25eff57c3872e9fe9ca7d08fe11c536730835
viclomaro <viclomaro@gmail.com>
2020-08-28 19:36:49 +0200

    conexión con la api de tiempo y vista html de estado, temperaturas y humedad

--- wearweather/src/app/api.service.ts
+++ wearweather/src/app/api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  baseUrl: string;
+  constructor(private http: HttpClient) {
+    this.baseUrl = 'https://api.tutiempo.net/json/?lan=es&apid=4sTzqXX4Xz4zM7v&lid=3768'
+  }
+
+  getWeather(): Promise<any[]> {
+    return this.http.get<any[]>(this.baseUrl).toPromise();
+  }
 }
