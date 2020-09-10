5ee268b86ed9604130d3823f43fada79b61c02a9
viclomaro <viclomaro@gmail.com>
2020-09-10 14:34:23 +0200

    switch con rangos de temperaturas con imagenes asignadas

--- wearweather/src/app/home/home.component.ts
+++ wearweather/src/app/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit {
     this.tempMin = this.arrWeather?.day1.temperature_min;
     /* get temperature */
     this.tempValue(this.tempMax, this.tempMin);
-    this.weatherTemp(39);
+    this.weatherTemp(this.temp);
   }
 
   tempValue(tempMax, tempMin): void {
@@ -35,24 +35,67 @@ export class HomeComponent implements OnInit {
   }
 
   weatherTemp(temperature): void {
-    if (temperature >= 60) {
-      this.imgWoman = "../../assets/images/women/women spring3 spr.jpg";
-      this.imgMan = "../../assets/images/men/men spring 3 spr.jpg";
-    } else if (temperature > 45 && temperature < 60) {
-      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
-      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
-    } else if (temperature > 30 && temperature < 45) {
-      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
-      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
-    } else if (temperature > 15 && temperature < 30) {
-      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
-      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
-    } else if (temperature > 10 && temperature < 15) {
-      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
-      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
-    } else if (temperature > 0 && temperature < 10) {
-      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
-      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
+    switch (true) {
+      /* summer */
+      case temperature >= 80:
+        this.imgWoman = "../../assets/images/women/summer/women summer 80+.jpg";
+        this.imgMan = "../../assets/images/men/summer/men summer 80+.jpg";
+        break;
+      case temperature >= 70 && temperature < 80:
+        this.imgWoman = "../../assets/images/women/summer/women summer 70-80.jpg";
+        this.imgMan = "../../assets/images/men/summer/men summer 70-80.jpg";
+        break;
+      case temperature >= 65 && temperature < 70:
+        this.imgWoman = "../../assets/images/women/summer/women summer 65-70.jpg";
+        this.imgMan = "../../assets/images/men/summer/men summer 65-70.jpg";
+        break;
+      case temperature >= 60 && temperature < 65:
+        this.imgWoman = "../../assets/images/women/summer/women summer 60-65.jpg";
+        this.imgMan = "../../assets/images/men//summer/men summer 60-65.jpg";
+        break;
+      /* spring */
+      case temperature >= 50 && temperature < 60:
+        this.imgWoman = "../../assets/images/women/spring/women spring 50-60.jpg";
+        this.imgMan = "../../assets/images/men/spring/men spring 50-60.jpg";
+        break;
+      case temperature >= 45 && temperature < 50:
+        this.imgWoman = "../../assets/images/women/spring/women spring 45-50.jpg";
+        this.imgMan = "../../assets/images/men/spring/men spring 45-50.jpg";
+        break;
+      case temperature >= 35 && temperature < 45:
+        this.imgWoman = "../../assets/images/women/spring/women spring 35-45.jpg";
+        this.imgMan = "../../assets/images/men/spring/men spring 35-45.jpg";
+        break;
+      /* fall */
+      case temperature >= 30 && temperature < 35:
+        this.imgWoman = "../../assets/images/women/fall/women fall 30-35.jpg";
+        this.imgMan = "../../assets/images/men/fall/men fall 30-35.jpg";
+        break;
+      case temperature >= 25 && temperature < 30:
+        this.imgWoman = "../../assets/images/women/fall/women fall 25-30.jpg";
+        this.imgMan = "../../assets/images/men/fall/men fall 25-30.jpg";
+        break;
+      case temperature >= 20 && temperature < 25:
+        this.imgWoman = "../../assets/images/women/fall/women fall 20-25.jpg";
+        this.imgMan = "../../assets/images/men/fall/men fall 20-25.jpg";
+        break;
+      /* winter */
+      case temperature >= 15 && temperature < 20:
+        this.imgWoman = "../../assets/images/women/winter/women winter 15-20.jpg";
+        this.imgMan = "../../assets/images/men/winter/men winter 15-20.jpg";
+        break;
+      case temperature >= 10 && temperature < 15:
+        this.imgWoman = "../../assets/images/women/winter/women winter 10-15.jpg";
+        this.imgMan = "../../assets/images/men/winter/men winter 10-15.jpg";
+        break;
+      case temperature >= 5 && temperature < 10:
+        this.imgWoman = "../../assets/images/women/winter/women winter 5-10.jpg";
+        this.imgMan = "../../assets/images/men/winter/men winter 5-10.jpg";
+        break;
+      case temperature < 0:
+        this.imgWoman = "../../assets/images/women/winter/women winter 0.jpg";
+        this.imgMan = "../../assets/images/men/winter/men winter 0.jpg";
+        break;
     }
 
 
