3df4d88574c30e849e7a0f97bd4dfe568ffedc11
viclomaro <viclomaro@gmail.com>
2020-09-10 13:39:05 +0200

    Funciones para obtener temperatura media, funcion para obtener imagen segun temperatura

--- wearweather/src/app/home/home.component.ts
+++ wearweather/src/app/home/home.component.ts
@@ -9,12 +9,53 @@ import { ApiService } from '../api.service';
 export class HomeComponent implements OnInit {
 
   arrWeather;
+  imgMan: string;
+  imgWoman: string;
+  tempMax: number;
+  tempMin: number;
+  temp: number;
 
-  constructor(private ApiService: ApiService) { }
+  constructor(private ApiService: ApiService) {
+
+  }
 
   async ngOnInit() {
+    /* call to api */
     this.arrWeather = await this.ApiService.getWeather();
-    console.log(this.arrWeather);
+    /* assign temperatures */
+    this.tempMax = this.arrWeather?.day1.temperature_max;
+    this.tempMin = this.arrWeather?.day1.temperature_min;
+    /* get temperature */
+    this.tempValue(this.tempMax, this.tempMin);
+    this.weatherTemp(39);
+  }
+
+  tempValue(tempMax, tempMin): void {
+    this.temp = (tempMax + tempMin);
+  }
+
+  weatherTemp(temperature): void {
+    if (temperature >= 60) {
+      this.imgWoman = "../../assets/images/women/women spring3 spr.jpg";
+      this.imgMan = "../../assets/images/men/men spring 3 spr.jpg";
+    } else if (temperature > 45 && temperature < 60) {
+      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
+      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
+    } else if (temperature > 30 && temperature < 45) {
+      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
+      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
+    } else if (temperature > 15 && temperature < 30) {
+      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
+      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
+    } else if (temperature > 10 && temperature < 15) {
+      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
+      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
+    } else if (temperature > 0 && temperature < 10) {
+      this.imgWoman = "../../assets/images/women/women spring2 spr.jpg";
+      this.imgMan = "../../assets/images/men/men spring 2 spr.jpg";
+    }
+
+
   }
 
 
