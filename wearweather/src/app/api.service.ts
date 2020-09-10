4faf31a854b8bef403c42423dfc3c78b1aa3daeb
viclomaro <viclomaro@gmail.com>
2020-09-10 17:43:32 +0200

    Depurada función para obtención de imágenes sin switch

--- wearweather/src/app/api.service.ts
+++ wearweather/src/app/api.service.ts
@@ -7,11 +7,14 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
   baseUrl: string;
+
+
   constructor(private http: HttpClient) {
-    this.baseUrl = 'https://api.tutiempo.net/json/?lan=es&apid=4sTzqXX4Xz4zM7v&lid=3768'
+    this.baseUrl = 'https://api.tutiempo.net/json/?lan=es&apid=4sTzqXX4Xz4zM7v&lid=3768';
   }
 
   getWeather(): Promise<any[]> {
     return this.http.get<any[]>(this.baseUrl).toPromise();
   }
+
 }
--- wearweather/src/app/home/home.component.ts
+++ wearweather/src/app/home/home.component.ts
@@ -14,92 +14,62 @@ export class HomeComponent implements OnInit {
   tempMax: number;
   tempMin: number;
   temp: number;
+  dirWoman: string;
+  dirMan: string;
+  urlIcon: string;
+  icon: string;
 
   constructor(private ApiService: ApiService) {
+    this.dirWoman = "../../assets/images/women/";
+    this.dirMan = "../../assets/images/men/";
+    this.urlIcon = "https://v5i.tutiempo.net/wi/02/50/";
+  }
+
+  ngOnInit() {
+    this.getWeather();
 
   }
 
-  async ngOnInit() {
-    /* call to api */
+  async getWeather() {
+    // call to api 
     this.arrWeather = await this.ApiService.getWeather();
-    /* assign temperatures */
+    // Set url icon
+    this.setFormattedUrlIcon();
+    // assign temperatures 
+    this.setTemperatures();
+    // get temperature 
+    this.setTempValue();
+    this.setWeatherTemp();
+
+  }
+
+
+  setTemperatures(): void {
     this.tempMax = this.arrWeather?.day1.temperature_max;
     this.tempMin = this.arrWeather?.day1.temperature_min;
-    /* get temperature */
-    this.tempValue(this.tempMax, this.tempMin);
-    this.weatherTemp(this.temp);
   }
 
-  tempValue(tempMax, tempMin): void {
-    this.temp = (tempMax + tempMin);
+
+  setFormattedUrlIcon(): void {
+    this.icon = this.arrWeather?.day1.icon;
+    this.urlIcon = `${this.urlIcon}${this.icon}.png`;
   }
 
-  weatherTemp(temperature): void {
-    switch (true) {
-      /* summer */
-      case temperature >= 80:
-        this.imgWoman = "../../assets/images/women/summer/women summer 80+.jpg";
-        this.imgMan = "../../assets/images/men/summer/men summer 80+.jpg";
-        break;
-      case temperature >= 70 && temperature < 80:
-        this.imgWoman = "../../assets/images/women/summer/women summer 70-80.jpg";
-        this.imgMan = "../../assets/images/men/summer/men summer 70-80.jpg";
-        break;
-      case temperature >= 65 && temperature < 70:
-        this.imgWoman = "../../assets/images/women/summer/women summer 65-70.jpg";
-        this.imgMan = "../../assets/images/men/summer/men summer 65-70.jpg";
-        break;
-      case temperature >= 60 && temperature < 65:
-        this.imgWoman = "../../assets/images/women/summer/women summer 60-65.jpg";
-        this.imgMan = "../../assets/images/men//summer/men summer 60-65.jpg";
-        break;
-      /* spring */
-      case temperature >= 50 && temperature < 60:
-        this.imgWoman = "../../assets/images/women/spring/women spring 50-60.jpg";
-        this.imgMan = "../../assets/images/men/spring/men spring 50-60.jpg";
-        break;
-      case temperature >= 45 && temperature < 50:
-        this.imgWoman = "../../assets/images/women/spring/women spring 45-50.jpg";
-        this.imgMan = "../../assets/images/men/spring/men spring 45-50.jpg";
-        break;
-      case temperature >= 35 && temperature < 45:
-        this.imgWoman = "../../assets/images/women/spring/women spring 35-45.jpg";
-        this.imgMan = "../../assets/images/men/spring/men spring 35-45.jpg";
-        break;
-      /* fall */
-      case temperature >= 30 && temperature < 35:
-        this.imgWoman = "../../assets/images/women/fall/women fall 30-35.jpg";
-        this.imgMan = "../../assets/images/men/fall/men fall 30-35.jpg";
-        break;
-      case temperature >= 25 && temperature < 30:
-        this.imgWoman = "../../assets/images/women/fall/women fall 25-30.jpg";
-        this.imgMan = "../../assets/images/men/fall/men fall 25-30.jpg";
-        break;
-      case temperature >= 20 && temperature < 25:
-        this.imgWoman = "../../assets/images/women/fall/women fall 20-25.jpg";
-        this.imgMan = "../../assets/images/men/fall/men fall 20-25.jpg";
-        break;
-      /* winter */
-      case temperature >= 15 && temperature < 20:
-        this.imgWoman = "../../assets/images/women/winter/women winter 15-20.jpg";
-        this.imgMan = "../../assets/images/men/winter/men winter 15-20.jpg";
-        break;
-      case temperature >= 10 && temperature < 15:
-        this.imgWoman = "../../assets/images/women/winter/women winter 10-15.jpg";
-        this.imgMan = "../../assets/images/men/winter/men winter 10-15.jpg";
-        break;
-      case temperature >= 5 && temperature < 10:
-        this.imgWoman = "../../assets/images/women/winter/women winter 5-10.jpg";
-        this.imgMan = "../../assets/images/men/winter/men winter 5-10.jpg";
-        break;
-      case temperature < 0:
-        this.imgWoman = "../../assets/images/women/winter/women winter 0.jpg";
-        this.imgMan = "../../assets/images/men/winter/men winter 0.jpg";
-        break;
-    }
+  setTempValue(): void {
+    this.temp = this.tempMax + this.tempMin;
+  }
 
-
+  setWeatherTemp(): void {
+    const tempString = this.temp.toString();
+    const tempFirstNumber = parseInt(tempString.charAt(0));
+    const tempSecondNumber = parseInt(tempString.charAt(1));
+    const imageNumber = tempSecondNumber > 5 ? 5 : 0;
+    this.imgWoman = `${this.dirWoman}${tempFirstNumber}${imageNumber}.jpg`;
+    this.imgMan = `${this.dirMan}${tempFirstNumber}${imageNumber}.jpg`;
   }
 
 
 }
+
+
+
