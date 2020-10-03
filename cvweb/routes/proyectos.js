781f378aca33e92ac8786d6374bac381e9111655
viclomaro <viclomaro@gmail.com>
2020-10-03 14:00:50 +0200

    peticion update funcionando.

--- cvweb/routes/proyectos.js
+++ cvweb/routes/proyectos.js
@@ -15,6 +15,7 @@ router.get('/new', async (req, res) => {
 });
 
 router.get('/edit/:proyectoId', async (req, res) => {
+    //TODO hacer try catch
     const proyecto = await Proyecto.findById(req.params.proyectoId).lean();
     res.render('proyectos/formEdit', { proy: proyecto });
 }
@@ -35,10 +36,24 @@ router.post('/create', upload.single('imagen'), async (req, res) => {
 
 });
 
+router.post('/update', upload.single('imagen'), async (req, res) => {
+    //TODO obtener categoria (helpers handlebars)    
+    const finalPath = req.file.path + '.' + mimeTypeExtension(req.file.mimetype);
+    fs.renameSync(req.file.path, finalPath);
+    console.log(req.file);
+
+    req.body.imagen = finalPath;
+    try {
+        await Proyecto.findByIdAndUpdate(req.body.proyectoId, req.body);
+        res.redirect('/proyectos');
+    } catch (err) {
+        res.json({ error: err });
+    }
+});
 
 
 function mimeTypeExtension(mimeType) {
-    return mimeType.split('/')[0];
+    return mimeType.split('/')[1];
 }
 
 module.exports = router;
