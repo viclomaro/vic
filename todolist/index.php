e5b4f2fc238c7fd09cf1036e10a5654e16a5fe2c
viclomaro <viclomaro@gmail.com>
2020-11-18 16:49:14 +0100

    Añadir tarea

--- todolist/index.php
+++ todolist/index.php
@@ -2,6 +2,7 @@
 
 include_once 'conexion.php';
 
+// Leer tareas
 $sql_leer = 'SELECT * FROM tareas';
 
 $gsent = $pdo->prepare($sql_leer);
@@ -9,7 +10,24 @@ $gsent->execute();
 
 $tareas = $gsent->fetchAll();
 
-var_dump($tareas);
+// var_dump($tareas);
+
+// Añadir tareas
+if($_POST){
+  $tarea = $_POST['tarea'];
+  $descripcion = $_POST['descripcion'];
+
+  $sqlInsertar = 'INSERT INTO tareas (tarea, descripcion) VALUES (?,?)';
+
+  $sentenciaAgregar = $pdo->prepare($sqlInsertar);
+  $sentenciaAgregar->execute(array($tarea, $descripcion));
+
+  // Refrescamos de nuevo la página
+  header('location:index.php');
+
+}
+
+
 
 ?>
 
@@ -28,7 +46,7 @@ var_dump($tareas);
   <body>
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-6">
+          <div class="col-md-6">
 
             <?php foreach($tareas as $tarea): ?>
 
@@ -39,20 +57,27 @@ var_dump($tareas);
                 </div>
 
                 <?php endforeach ?>
-            </div>
+          </div>
+
+          <div class="col-md-6">
+              <h2>Añadir Tareas</h2>
+              <form method="POST">
+                <input type="text" class="form-control" name="tarea" placeholder="Tarea">
+                <input type="text" class="form-control mt-3" name="descripcion" placeholder="Descripción">
+                <button class="btn btn-primary mt-3">Añadir</button>
+              </form>
+          </div>
         </div>
     </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+
+
+
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
